2de2b88028f613c97c1968f599a9fbc5114c0b35
Dzmitry Ihnatovich <ignatovich.dm@gmail.com>
2023-09-16 15:01:39 +0200

    Update tree.js

--- tree.js
+++ tree.js
@@ -130,7 +130,6 @@ class Node {
         }
       }
     }
-      //BFS
     breadthFirstSearch() {
       let currentNode = this.root;
       const list = [];
@@ -169,6 +168,51 @@ class Node {
       return this.breadthFirstSearchRec(queue, list)
       
     }
+    DFSInOrder() {
+      return traverseInOrder(this.root, [])
+    }
+    DFSPostOrder() {
+      return traversePostOrder(this.root, [])
+    }
+    DFSPreOrder() {
+      return traversePreOrder(this.root, [])
+    }
+  }
+
+  function traverseInOrder(node, list) {
+    if(node.left){
+      traverseInOrder(node.left, list);
+    }
+    list.push(node.value);
+    if(node.right){
+      traverseInOrder(node.right, list);
+    }
+  
+    return list;
+  }
+  
+  function traversePostOrder(node, list) {
+    if(node.left){
+      traversePostOrder(node.left, list);
+    }
+    if(node.right){
+      traversePostOrder(node.right, list);
+    }
+    list.push(node.value);
+  
+    return list;
+  }
+  
+  function traversePreOrder(node, list) {
+    list.push(node.value);
+    if(node.left){
+      traversePreOrder(node.left, list);
+    }
+    if(node.right){
+      traversePreOrder(node.right, list);
+    }
+  
+    return list;
   }
   
   const tree = new BinarySearchTree();
